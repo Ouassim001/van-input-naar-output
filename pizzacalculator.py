29952f26ecd9f6dda951affabb375a974ddde214
Ouassim001 <89780864+Ouassim001@users.noreply.github.com>
2021-09-08 11:52:39 +0200

    Add files via upload

---
+++ pizzacalculator.py
@@ -0,0 +1,13 @@
+print("----------------------------------------------")
+
+print("pizza small kost 5$")
+print("pizza medium kost 8$")
+print("pizza large kost 10$")
+pizzasmall = int(input("hoeveel pizza small wil je? "))
+pizzamedium = int(input("hoeveel pizza medium wil je? "))
+pizzalarge = int(input("hoeveel pizza large wil je? "))
+pricesmall = (pizzasmall * 5)
+pricemedium = (pizzamedium * 8)
+pricelarge = (pizzalarge * 10)
+
+print("de prijs van uw pizza is dan " + str(pricesmall + pricemedium + pricelarge) + " euro")
